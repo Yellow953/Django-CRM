1f1f3a758fbe131771a2669c1de913648af2d2fd
Yellow <joemazloum953@gmail.com>
2023-10-20 16:11:05 +0300

    initial commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'qwe123',
+)
+
+cursor = database.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS CRM")
+
+print("Database created successfully")
